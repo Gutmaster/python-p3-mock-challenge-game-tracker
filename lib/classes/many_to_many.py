4f90da7c7bd2804f2147afde5c0270fc2078ace0
Chevy <vallchevy@gmail.com>
2024-06-24 22:21:05 -0300

    complete code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,90 @@
 class Game:
     def __init__(self, title):
-        self.title = title
+        self._title = title
+        self._results = []
+        self._players = []
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if hasattr(self, 'title'): return
+        if isinstance(title, str) and title != "":
+            self._title = title
+        else:
+            raise Exception("Title must be a non-empty string")
 
     def results(self):
-        pass
+        return self._results
 
     def players(self):
-        pass
+        return self._players
 
     def average_score(self, player):
-        pass
+        total = 0
+        for result in self.results():
+            if result.player == player:
+                total += result.score
+        return total / len(self.results())
 
 class Player:
     def __init__(self, username):
-        self.username = username
+        self._username = username
+        self._results = []
+        self._games_played = []
 
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and 2 <= len(username) <= 16:
+            self._username = username
+        
     def results(self):
-        pass
+        return self._results
 
     def games_played(self):
-        pass
+        return self._games_played
 
     def played_game(self, game):
-        pass
+        return self in game.players()
 
     def num_times_played(self, game):
-        pass
+        gamecount = 0
+        for result in self.results():
+            if result in game.results():
+                gamecount += 1
+        return gamecount
+
 
 class Result:
+    all = []
+
     def __init__(self, player, game, score):
+        game._results.append(self)
+        player.results().append(self)
+        self._score = score
         self.player = player
         self.game = game
-        self.score = score
+
+        if not player in game._players:
+            game._players.append(player)
+            player._games_played.append(game)
+
+        Result.all.append(self)
+            
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def score(self, score):
+        if hasattr(self, 'score'): return
+        if isinstance(score, int) and 1 <= score <= 5000:
+            self._score = score
+        else:
+            raise Exception("Score must be an integer between 1 and 5000")
